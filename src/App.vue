6e7b3abe2710ca734e9fcb13352594022a5dcd8e
Solj <17673095+Soljdev@users.noreply.github.com>
2024-11-12 15:47:23 +0200

    Верстка подвала

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
   <div class="wrapper">
     <router-view />
   </div>
+  <footer class="max-w-screen-lg mx-auto px-4 ">
+    <div class="w-full border-t-[2px] border-t-[#222] mt-4 py-6 font-display text-sm flex flex-wrap justify-between">
+      <div><strong>© 2022 Сompany</strong>. Соглашение об обработке персональных данных и политика конфиденциальности</div>
+      <div>Сделано в <strong>Palladiumlab</strong></div>
+    </div>
+  </footer>
 </template>
 
 <script lang="ts">
@@ -15,3 +21,9 @@
     },
   })
 </script>
+
+<style>
+footer {
+
+}
+</style>
